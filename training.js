d04af43f4f05b33e6ca160b25bcf4964c057e855
Krishna191919 <krishnachaudhary191919@gmail.com>
2025-01-01 22:18:46 +0545

    Update training.js
    Second answer

--- training.js
+++ training.js
@@ -4,11 +4,15 @@ Homework(JS-Training)
     * Marks >= 75: B
     * Marks >= 50: C
     * Else: F
+  ANSWER👇:
 Let marks=78 ;
 if(marks>=90){console.log("A Grade!!");
 } else if (marks>=75){console.log("B Grade!!");} else if (marks>=){console.log(" C Grade!!");} else {console.log("Fail!!");}
  
 
 2. Write a program that takes a year as input and checks whether it is a leap year.
+ANSWER👇:
+Let year=2020;
+if(year%4==0){console.log("it's leap year");} else{console.log("it's not leap year")}
 
 3. Write a program to simulate a simple calculator using a switch statement. The user enters two numbers and an operator (+, -, *, /).
