435f2daf86433b94b9b06f7688e52e24b1670177
Krishna191919 <krishnachaudhary191919@gmail.com>
2025-01-01 22:04:56 +0545

    Update questions

---
+++ training.js
@@ -0,0 +1,10 @@
+Homework(JS-Training)
+1. Write a program to determine the grade of a student based on their marks:
+    * Marks >= 90: A
+    * Marks >= 75: B
+    * Marks >= 50: C
+    * Else: F
+
+2. Write a program that takes a year as input and checks whether it is a leap year.
+
+3. Write a program to simulate a simple calculator using a switch statement. The user enters two numbers and an operator (+, -, *, /).
