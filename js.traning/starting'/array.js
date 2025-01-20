2626671dd2fca4998bda6db5a3e654c50dc5f05e
Krishna191919 <krishnachaudhary191919@gmail.com>
2025-01-20 14:21:26 +0545

    Add files via upload

---
+++ js.traning/starting'/array.js
@@ -0,0 +1,29 @@
+// Array are collection of element of with different data types !!
+// Array are mutable in nature!!
+
+// syntax 
+// const var_name= [item 1,item2,item3.....item n]
+
+//example:
+// const array_item =['fruit','vegetable',200,1.5,true]
+// console.log(array_item[0]);
+// console.log(array_item[1]);
+// console.log(array_item[2]);
+// console.log(array_item[3]);
+// console.log(array_item[4]);
+
+// let array=[];
+// array[0]="fruit";
+// array[1]=123;
+// array[2]=12.5;
+// array[3]=true
+// array[4]='vegetable';
+// console.log(array);
+
+// // Array methodes!!
+// 1.push 
+let course = ['c','sd','is']
+course.push("isp");
+console.log(course);
+
+
