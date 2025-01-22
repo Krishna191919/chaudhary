8a3bcd9c3ab94061c074b5104c03523a247187fc
Krishna191919 <krishnachaudhary191919@gmail.com>
2025-01-22 14:29:20 +0545

    Add files via upload

---
+++ js.traning/function/function.js
@@ -0,0 +1,21 @@
+// function declaration
+function greet(name1,name2){
+    console.log("hello "+ name1+name2);
+    
+} greet("krishna sir ","sanjog sir");
+
+
+// sum two number
+function sum(a,b){
+    console.log(a+b);
+} 
+sum(10,20);
+sum(30,20);
+
+// multiplication num
+function multi(a,b){
+    return a*b;
+} 
+let a=multi.toString(10,20);
+console.log(a);
+
