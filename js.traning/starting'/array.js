095d32a93b97f9c5fc5e3ad3ecc94c6877ea5c0f
Krishna191919 <krishnachaudhary191919@gmail.com>
2025-01-22 14:33:48 +0545

    Update array.js

--- js.traning/starting'/array.js
+++ js.traning/starting'/array.js
@@ -32,8 +32,37 @@
 // console.log(course);
 
 // 3.concat
+//concat methode
+// let fruits=[350];
+// let newarray=array_item.concat(fruits);
+// console.log(newarray);
+
 // 4.splice
+//removing elements
+let fruits0 = ['apple', 'banana', 'cherry', 'date'];
+fruits.splice(1, 2); // Removes 2 elements starting at index 1
+console.log(fruits); // ['apple', 'date']
+
+// adding elements
+let fruits1 = ['apple', 'banana', 'cherry', 'date'];
+fruits.splice(2, 0, 'kiwi', 'lemon'); // Adds 'kiwi' and 'lemon' at index 2 without removing any elements
+console.log(fruits); // ['apple', 'banana', 'kiwi', 'lemon', 'cherry', 'date']
+
+// replacing elements
+let fruits2 = ['apple', 'banana', 'cherry', 'date'];
+fruits.splice(1, 2, 'kiwi', 'lemon'); // Removes 2 elements starting at index 1 and adds 'kiwi' and 'lemon'
+console.log(fruits); // ['apple', 'kiwi', 'lemon', 'date']
+
+// removing elements from the starting of number!!
+let fruits3 = ['apple', 'banana', 'cherry', 'date'];
+fruits.splice(1); // Removes all elements starting from index 1
+console.log(fruits); // ['apple']
+
 // 5.slice
+const fruit=['apple','banana','mango','pineapple'];
+const array=fruit.slice(1,3);// print starting value from 1 and befor ending 3!! 
+console.log(array);
+
 // 6.shift
 const delitem1 =['fruit','vegetable',200,1.5,true];
 delitem1.shift();// delete items from first value of array
