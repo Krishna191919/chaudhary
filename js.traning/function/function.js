f6d207476de1bf9a3bb80a28f6034984573884c5
Krishna191919 <krishnachaudhary191919@gmail.com>
2025-01-23 14:20:09 +0545

    Update function.js

--- js.traning/function/function.js
+++ js.traning/function/function.js
@@ -1,4 +1,4 @@
-// function declaration
+// 1.function declaration
 function greet(name1,name2){
     console.log("hello "+ name1+name2);
     
@@ -18,4 +18,56 @@ function multi(a,b){
 } 
 let a=multi.toString(10,20);
 console.log(a);
+
+
+/ 2.function expression 
+// name(); // call function 
+// function name (){
+//     console.log("hello world");
+// }
+// // wap to check the number is even or odd!!
+// function evenorodd1 (a){
+//     if (a%2===0){
+//         console.log(`${a} is even number`);
+//     } else{
+//         console.log(`${a} is odd number`);
+//     }
+// } evenorodd1(15);
+// //        OR
+// function evenorodd2(b){
+//     if (b%2===0){
+//         return`${b} is even number`;
+// } else{
+//     return`${b} is oddd number`;
+// }
+// } let result=evenorodd2(10);
+// console.log(result);
+//       OR
+
+// const greet = function(name) {
+//     console.log("Hello, " + name + "!");
+//   };
+  
+//   greet("Lumbini"); // Output: Hello, Lumbini!
+  
+
+// 3. arrow function
+// const greet1 = (name) => {
+//     console.log("Hello, " + name + "!");
+//   };
+  
+//   greet1("Lumbini"); // Output: Hello, Lumbini!
+
+  //4. higher order function
+  function greet3(name) {
+    name="Hello"+ name ;
+  }
+  
+  function processUserInput(callback) {
+    let name = "Lumbini";
+    console.log(callback(name));
+  }
+  processUserInput(greet3); // Output: Hello, Lumbini!
+  
+  
 
